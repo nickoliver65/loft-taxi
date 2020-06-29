9dc3b2eaf6945164d68e0123d27572e74b3d191a
Sergey <46181227+nickoliver65@users.noreply.github.com>
2020-06-29 18:24:17 +0700

    less 1 rev 2

--- src/Registration.jsx
+++ src/Registration.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import {ActionBtn} from './App.js'
 
-export const Registration = () =>
+export const Registration = (props) =>
 {
     return <>
     <form>
@@ -8,7 +9,8 @@ export const Registration = () =>
         <input type = 'email' name = 'email' size = '30' placeholder = 'Адрес электронной почты'></input>       
         <input type = 'text' name = 'firstname' size = '30' placeholder = 'Имя'></input>       
         <input type = 'text' name = 'lastname' size = '30' placeholder = 'Фамилия'></input>       
-        <input type = 'password' name = 'password' size = '30' placeholder = 'Пароль'></input>       
+        <input type = 'password' name = 'password' size = '30' placeholder = 'Пароль'></input>  
+        <ActionBtn placeholder = {"Вход"} onClick = {()=>{props.navigateTo("map")}}/>     
     </form>
     </>
 }
